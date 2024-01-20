be13bc5ea1d71ad39fb1250b574cfc7132309748
Dylan Garza <DAG.Gamer999@gmail.com>
2024-01-20 00:09:40 -0600

    Basic World Collidable objects

--- Untitled Wizard Game/main.cpp
+++ Untitled Wizard Game/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <vector>
 #include <raylib.h>
 #include <raymath.h>
 
 //in the beginning god created the heavens and the earth (START)
 /*
-* make a basic plane to walk on
-* add a player
-* add collision objects
-* grid align the objects
-* these objects will be "blocks", and the grid will be made of "tiles"
+* make a basic plane to walk on ...........................................................DONE
+* add a player ............................................................................DONE
+* add collision objects ...................................................................DONE
+* grid align the objects ..................................................................DONE
+* these objects will be "blocks", and the grid will be made of "tiles" ....................WOOHOO!
 */
 
 //And so man learned to create (BLOCKS)
@@ -28,16 +29,111 @@
 * make items do different things
 */
 
+class Player {
+	Rectangle hitbox{ 0, 0, 25, 25 };
+	std::vector<std::vector<Rectangle>>* collisionObjects;
+
+public:
+	Player(std::vector<std::vector<Rectangle>>* objects) {
+		collisionObjects = objects;
+	}
+
+	void move() {
+		if (IsKeyDown(KeyboardKey::KEY_W)) {
+			hitbox.y -= 5;
+
+			for (auto& row : *collisionObjects) {
+				for (auto& r : row) {
+					if (CheckCollisionRecs(hitbox, r)) {
+						hitbox.y += 5;
+						break;
+					}
+				}
+			}
+		}
+		if (IsKeyDown(KeyboardKey::KEY_A)) {
+			hitbox.x -= 5;
+
+			for (auto& row : *collisionObjects) {
+				for (auto& r : row) {
+					if (CheckCollisionRecs(hitbox, r)) {
+						hitbox.x += 5;
+						break;
+					}
+				}
+			}
+		}
+		if (IsKeyDown(KeyboardKey::KEY_S)) {
+			hitbox.y += 5;
+
+			for (auto& row : *collisionObjects) {
+				for (auto& r : row) {
+					if (CheckCollisionRecs(hitbox, r)) {
+						hitbox.y -= 5;
+						break;
+					}
+				}
+			}
+		}
+		if (IsKeyDown(KeyboardKey::KEY_D)) {
+			hitbox.x += 5;
+
+			for (auto& row : *collisionObjects) {
+				for (auto& r : row) {
+					if (CheckCollisionRecs(hitbox, r)) {
+						hitbox.x -= 5;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	void draw() {
+		DrawRectangleRec(hitbox, RED);
+	}
+};
+
 int main() {
 	InitWindow(800, 800, "Untitled Wizard Game");
 	SetTargetFPS(60);
+
+	std::vector<std::vector<Rectangle>> boxGrid;
+
+	boxGrid.resize(25);
+	for (auto& row : boxGrid) {
+		row.resize(25);
+	}
+
+	Player player(&boxGrid);
 	
+	for (int y = 0; y < 25; y++) {
+		for (int x = 0; x < 25; x++) {
+			if (GetRandomValue(0, 3) == 0) {
+				boxGrid.at(y).at(x) = { (float)x * 25.0f, (float)y * 25.0f, 25.0f, 25.0f };
+			}
+		}
+	}
+
 	while (!WindowShouldClose()) {
 		//game logic
-
+		player.move();
 
 		//draw
 		BeginDrawing();
+		ClearBackground(DARKGREEN);
+
+		player.draw();
+
+		for (auto& row : boxGrid) {
+			for (auto& r : row) {
+				DrawRectangleRec(r, DARKBLUE);
+			}
+		}
+
+		/*for (auto& r : boxes) {
+			DrawRectangleRec(r, DARKBLUE);
+		}*/
 
 		EndDrawing();
 	}
