37a54d8b631dfe372c735dd97499aa20b5e611eb
Dylan Garza <DAG.Gamer999@gmail.com>
2024-01-20 12:17:35 -0600

    Tilemap implemented

--- Untitled Wizard Game/main.cpp
+++ Untitled Wizard Game/main.cpp
@@ -14,6 +14,9 @@
 
 //And so man learned to create (BLOCKS)
 /*
+* Create distinction between tiles and blocks
+*	TILE: invisible, can be occupied, solid, stores root of block
+*	BLOCK: visible, interactable
 * make blocks be able to be colored
 * make blocks able to take up multiple tiles
 * make blocks placeable
@@ -28,6 +31,90 @@
 * make items
 * make items do different things
 */
+
+class Block {
+	Color clr{ BLACK };
+	int tileWidth{1};
+	int tileHeight{1};
+};
+
+struct Tile {
+	Rectangle hitbox{ 0.0f, 0.0f, 0.0f, 0.0f };
+	bool occupied{0};
+	bool solid{0};
+};
+
+class Tilemap {
+	std::vector<std::vector<Tile>> grid;
+	int tileDims = 0;
+
+public:
+	Tilemap() {
+
+	}
+
+	Tilemap(int gridDims, int tileDims) {
+		set_Dims(gridDims);
+
+		//grid align tiles
+		for (int y = 0; y < gridDims; y++) {
+			for (int x = 0; x < gridDims; x++) {
+				at(x, y).hitbox = { (float)(x * tileDims), (float)(y * tileDims), (float)tileDims, (float)tileDims };
+			}
+		}
+	}
+
+	//can only be set if hasnt been set before
+	void set_Dims(int gridDims) {
+		grid.resize(gridDims);
+		for (auto& row : grid) {
+			row.resize(gridDims);
+		}
+	}
+
+	Tile& at(int x, int y) {
+		return grid.at(y).at(x);
+	}
+
+	int get_dims() {
+		return grid.size();
+	}
+
+	void set_occupy(int x, int y, bool state) {
+		at(x, y).occupied = state;
+	}
+
+	void set_solid(int x, int y, bool state) {
+		at(x, y).solid = state;
+	}
+
+	/*void randomize(double p) {
+		for (int y = 0; y < 25; y++) {
+			for (int x = 0; x < 25; x++) {
+				if (GetRandomValue(0, 3) == 0) {
+					boxGrid.at(y).at(x) = { (float)x * 25.0f, (float)y * 25.0f, 25.0f, 25.0f };
+				}
+			}
+		}
+	}*/
+
+	/*void drawAll() {
+		for (auto& row : grid) {
+			for (auto& r : row) {
+				DrawRectangleRec(r, DARKBLUE);
+			}
+		}
+	}*/
+};
+
+class Blockmap {
+	Tilemap tiles;
+	
+public:
+	Blockmap() {
+
+	}
+};
 
 class Player {
 	Rectangle hitbox{ 0, 0, 25, 25 };
